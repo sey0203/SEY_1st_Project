a9dfac21a0bdfb4cb25f4b3293b4d87e0cf041d9
박재윤 <lossers0@gmail.com>
2025-03-25 12:16:38 +0900

    Readme.md 수정

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium_stealth import stealth
 
 
 user_agents = [
