bce46d5ce68e77d8639ca67f64b9c19aed28cfd7
sey0203 <hok4070@gmail.com>
2025-03-27 22:51:40 +0900

    25.03.27 마무리

--- pages/team_feed_page.py
+++ pages/team_feed_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+from pages.team_feed_page_xpath import TeamFeedPageXPaths
 
 
 
@@ -24,34 +25,19 @@ class BasePage:
 
 class TeamFeedPage(BasePage):
     URL = "https://kdt-pt-1-pj-2-team03.elicecoding.com/teams/1"
-    GNB_SELECTOR = "div.fixed.bottom-0.w-full.z-50.max-w-\\[600px\\].border-t-\\[1px\\].bg-white.border-gray-200.p-3.h-16"
-    TEAM_FEED_SELECTOR = "a[href='/teams/1']"
-    COMBOBOX_SELECTOR = "button[role='combobox']"
-    TEAM_STATS_IMAGE_SELECTOR = "span.font-bold.text-sub-2.text-title"
-    COMBOBOX_TEAM_XPATH = "//span[contains(text(), '개발 1팀')]"
-    PROFILE_EDIT_ICON_SELECTOR = "div.flex.items-center.justify-between.text-subbody > svg.cursor-pointer"
-    PROFILE_EDIT_XPATH = "//span[contains(text(), '프로필 정보 수정')]"
-    TEAM_STATS_XPATH = "//span[contains(text(), '팀 통계')]"
-    TEAM_EATEN_MENU_XPATH = "//span[contains(text(), '팀이 먹은 메뉴')]"
-    PROFILE_EDIT_FINISH_BTN_SELECTOR = "button.cursor-pointer[type='submit']"
-    PROFILE_EDIT_X_BTN_SELECTOR = "button.text-2xl.cursor-pointer"
-    TEAM_EATEN_MENU_ADD_BTN_SELECTOR = "div.flex.items-center.gap-4 > button"
-    ADD_REVIEW_XPATH = "//span[text()='새로운 후기 등록하기']"
-    NEW_REVIEW_FINISH_BTN_SELECTOR = "button:contains('후기 작성 완료')"
-
 
 
     def team_feed_click(self):
         #팀 피드 페이지 클릭 후 URL 변경까지 자동 대기
-        self.wait_for_element(By.CSS_SELECTOR, self.GNB_SELECTOR)
-        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.TEAM_FEED_SELECTOR)))
-        self.driver.find_element(By.CSS_SELECTOR, self.TEAM_FEED_SELECTOR).click()
+        self.wait_for_element(By.CSS_SELECTOR, TeamFeedPageXPaths.GNB_SELECTOR)
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, TeamFeedPageXPaths.TEAM_FEED_SELECTOR)))
+        self.driver.find_element(By.CSS_SELECTOR, TeamFeedPageXPaths.TEAM_FEED_SELECTOR).click()
         self.wait_for_url("teams")
 
     def combobox_team_select(self):
         #개발1팀 선택 후 URL 변경까지 자동 대기
         wait = ws(self.driver, 10)
-        team_option = wait.until(EC.element_to_be_clickable((By.XPATH, self.COMBOBOX_TEAM_XPATH)))
+        team_option = wait.until(EC.element_to_be_clickable((By.XPATH, TeamFeedPageXPaths.COMBOBOX_TEAM_XPATH)))
         ActionChains(self.driver).move_to_element(team_option).click().perform()
         self.wait_for_url("teams/1")
 
@@ -61,26 +47,33 @@ class TeamFeedPage(BasePage):
         element.click()
 
     def combobox_btn_click(self):
-        self.element_click(By.CSS_SELECTOR, self.COMBOBOX_SELECTOR)
+        self.element_click(By.CSS_SELECTOR, TeamFeedPageXPaths.COMBOBOX_SELECTOR)
 
     def profile_edit_icon_click(self):
-        self.element_click(By.CSS_SELECTOR, self.PROFILE_EDIT_ICON_SELECTOR)
+        self.element_click(By.CSS_SELECTOR, TeamFeedPageXPaths.PROFILE_EDIT_ICON_SELECTOR)
 
     def profile_edit_finish_btn_click(self):
-        self.element_click(By.CSS_SELECTOR, self.PROFILE_EDIT_FINISH_BTN_SELECTOR)
+        self.element_click(By.CSS_SELECTOR, TeamFeedPageXPaths.PROFILE_EDIT_FINISH_BTN_SELECTOR)
 
     def profile_edit_X_btn_click(self):       
         wait = ws(self.driver, 10)
-        x_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.PROFILE_EDIT_X_BTN_SELECTOR)))
+        x_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, TeamFeedPageXPaths.PROFILE_EDIT_X_BTN_SELECTOR)))
         self.driver.execute_script("arguments[0].click();", x_button)
 
     def team_eaten_menu_add_btn_click(self):       
         wait = ws(self.driver, 10)
-        add_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.TEAM_EATEN_MENU_ADD_BTN_SELECTOR)))
+        add_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, TeamFeedPageXPaths.TEAM_EATEN_MENU_ADD_BTN_SELECTOR)))
         self.driver.execute_script("arguments[0].click();", add_button)
 
     def new_review_finish_btn_click(self):
-        self.element_click(By.CSS_SELECTOR, self.NEW_REVIEW_FINISH_BTN_SELECTOR)
+        wait = ws(self.driver, 10)
+        finish_button = wait.until(EC.element_to_be_clickable((By.XPATH, TeamFeedPageXPaths.NEW_REVIEW_FINISH_BTN_XPATH)))
+        self.driver.execute_script("arguments[0].click();", finish_button)
+
+    def eat_same_menu_btn_click(self):
+        wait = ws(self.driver, 10)
+        eat_same_menu_button = wait.until(EC.element_to_be_clickable((By.XPATH, TeamFeedPageXPaths.EAT_SAME_MENU_BTN_XPATH)))
+        self.driver.execute_script("arguments[0].click();", eat_same_menu_button)
 
     def element_scroll(self, locator_type, locator):
         wait = ws(self.driver, 10)
@@ -88,10 +81,10 @@ class TeamFeedPage(BasePage):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
     def team_stats_scroll(self):
-        self.element_scroll(By.XPATH, self.TEAM_STATS_XPATH)
+        self.element_scroll(By.XPATH, TeamFeedPageXPaths.TEAM_STATS_XPATH)
 
     def team_eaten_menu_scroll(self):
-        self.element_scroll(By.XPATH, self.TEAM_EATEN_MENU_XPATH)
+        self.element_scroll(By.XPATH, TeamFeedPageXPaths.TEAM_EATEN_MENU_XPATH)
 
     def navigate_to_profile_edit(self, driver):
         #팀 피드에서 프로필 편집 화면으로 이동하는 공통 함수, 대기까지
@@ -101,7 +94,7 @@ class TeamFeedPage(BasePage):
         team_feed_page.combobox_team_select()
         team_feed_page.profile_edit_icon_click()
         wait = ws(driver, 10)
-        wait.until(EC.visibility_of_element_located((By.XPATH, self.PROFILE_EDIT_XPATH)))
+        wait.until(EC.visibility_of_element_located((By.XPATH, TeamFeedPageXPaths.PROFILE_EDIT_XPATH)))
 
     def navigate_to_team_eaten_menu(self, driver):
         #팀 피드에서 팀이 먹은 메뉴로 이동하는 공통 함수, 대기까지
@@ -111,7 +104,7 @@ class TeamFeedPage(BasePage):
         team_feed_page.combobox_team_select()
         team_feed_page.team_eaten_menu_scroll()
         wait = ws(driver, 10)
-        wait.until(EC.visibility_of_element_located((By.XPATH, self.TEAM_EATEN_MENU_XPATH)))
+        wait.until(EC.visibility_of_element_located((By.XPATH, TeamFeedPageXPaths.TEAM_EATEN_MENU_XPATH)))
 
     def navigate_to_add_review(self, driver):
         #팀 피드에서 새로운 후기 등록하기로 이동하는 공통 함수, 대기까지
@@ -119,4 +112,4 @@ class TeamFeedPage(BasePage):
         team_feed_page.navigate_to_team_eaten_menu(driver)
         team_feed_page.team_eaten_menu_add_btn_click()
         wait = ws(driver, 10)
-        wait.until(EC.presence_of_element_located((By.XPATH, self.ADD_REVIEW_XPATH)))
+        wait.until(EC.presence_of_element_located((By.XPATH, TeamFeedPageXPaths.ADD_REVIEW_TITLE_XPATH)))
