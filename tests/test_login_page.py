883d2b8d0ae80bac77e329b2f4d2957983fac302
P0RRU <47809076+P0RRU@users.noreply.github.com>
2025-03-31 14:40:02 +0900

    Update test_login_page.py

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -11,17 +11,7 @@ LoginData = {"username" : "qwer3@qwer.qwer", "password" : "qwerQWER1!","password
 FalseLogin = {"username1" : "hi@com", "password1" : "qwerqwer", "password2" : "12341234" }
 NewLoginData = {"username" : "qwer11@qwer.qwer", "password" : "qwerQWER1!"}
 
-
-def setup(login_page) :
-            login_page.open()
-            login_page.check_url("signin")
-            #로그인 버튼 클릭
-            login_page.click_button("로그인")
-            login_page.check_url("login")
-            
-            login_page.login(LoginData["username"], LoginData["password"])
-            login_page.check_url("welcome")
-
+@pytest.mark.skip
 @pytest.mark.usefixtures("driver")
 class TestLoginPage:
     @pytest.mark.skip
@@ -205,7 +195,7 @@ class TestLoginPage:
             "return window.getComputedStyle(arguments[0]).getPropertyValue('background-color');",
                 driver.find_element(By.CLASS_NAME, "cd059af9c, c1647f822")
             )
-            print(background_color)
+            
             if background_color == "rgb(255, 255, 255)" :
                 print(background_color)
                 assert True
@@ -377,7 +367,7 @@ class TestLoginPage:
                 "return window.getComputedStyle(arguments[0]).getPropertyValue('--page-background-color');",
                 driver.find_element(By.CLASS_NAME, "c3c4bf5c1, login")
             )
-            print(background_color)
+            
             if background_color == "#fff7ed" :
                 print(background_color)
                 assert True
@@ -562,6 +552,327 @@ class TestLoginPage:
             login_page.check_url("u/signup")
         
             
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_login_TC120(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+            
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+
+            background_color = driver.execute_script(
+            "return window.getComputedStyle(arguments[0]).getPropertyValue('background-color');",
+                driver.find_element(By.CLASS_NAME, "cd059af9c, c1647f822")
+            )
+            
+            if background_color == "rgb(255, 255, 255)" :
+                print(background_color)
+                assert True
+                return
+            assert False
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_login_TC121(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            #페이지 로딩
+            class_value="비밀번호를 잊어버리셨나요?"
+            get_text = driver.find_element(By.XPATH, f"//h1[contains(text(),'{class_value}')]").text
+
+            if class_value in get_text :
+                print(get_text)
+                assert True
+                return
+            assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_login_TC122(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            #페이지 로딩
+            class_value="이메일 주소를 입력하면 비밀번호 재설정 지침을 보내드립니다."
+            get_text = driver.find_element(By.XPATH, f"//p[contains(text(),'{class_value}')]").text
+
+            if class_value in get_text :
+                print(get_text)
+                assert True
+                return
+            assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC123_128(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.NAME, "email").send_keys(LoginData["username"])
+        except NoSuchElementException as e:
+                    print("이메일 인풋을 찾지 못함")
+                    assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC124(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.XPATH, '//button[contains(text(),"계속")]')
+        except NoSuchElementException as e:
+                    print("계속 버튼 찾지 못함")
+                    assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC125(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.XPATH, '//button[contains(text(),"로그인 화면으로 돌아가기")]')
+        except NoSuchElementException as e:
+                    print("로그인 화면 돌아가기 버튼 찾지 못함")
+                    assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+    
+    @pytest.mark.skip
+    def test_signup_TC126(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            background_color = driver.execute_script(
+                "return window.getComputedStyle(arguments[0]).getPropertyValue('--page-background-color');",
+                driver.find_element(By.CLASS_NAME, "c3c4bf5c1, login")
+            )
+            
+            if background_color == "#fff7ed" :
+                print(background_color)
+                assert True
+                return
+            assert False
+        except NoSuchElementException as e:
+                    print("계속 버튼 찾지 못함")
+                    assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC127(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.XPATH, '//button[contains(text(),"계속")]').click
+            
+            # 확인 로직
+            expected_focus_id = "email"
+            username_field = driver.find_element(By.ID, expected_focus_id)
+            error_massage=username_field.get_attribute("validationMessage")
+            active_element = driver.switch_to.active_element
+            
+            if active_element.get_attribute("id") == expected_focus_id:
+                if error_massage == "이 입력란을 작성하세요." :
+                    print(error_massage)
+                    print("✅ 테스트 성공: 필수 인풋으로 포커싱 이동함")
+                    assert True
+                    return
+            else:
+                print("❌ 테스트 실패: 예상한 필드로 포커싱되지 않음")
+            assert False
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+    
+    @pytest.mark.skip
+    def test_signup_TC129(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+            driver.find_element(By.NAME, "email").send_keys(FalseLogin["username1"])
+            driver.find_element(By.XPATH, '//button[contains(text(),"계속")]').click()
+            
+            try:
+                error_element = ws(driver, 2).until(
+                    EC.presence_of_element_located((By.ID, "error-element-email"))  
+                )
+                print(error_element.text)
+                return True
+            except TimeoutException:
+                assert False
+            
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC130(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.XPATH, '//button[contains(text(),"로그인 화면으로 돌아가기")]').click()
+
+
+        except NoSuchElementException as e:
+                    print("로그인 화면 돌아가기 버튼 찾지 못함")
+                    assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC131(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.NAME, "email").send_keys(LoginData["username"])
+            driver.find_element(By.XPATH, '//button[contains(text(),"계속")]').click()
+
+            class_value="Check Your Email"
+            get_text = driver.find_element(By.XPATH, f"//h1[contains(text(),'{class_value}')]").text
+
+            if class_value in get_text :
+                print(get_text)
+                assert True
+                return
+            
+            class_value1="Please check the email address"
+            get_text1 = driver.find_element(By.XPATH, f"//p[contains(text(),'{class_value}')]").text
+
+            if class_value1 in get_text1 :
+                print(get_text)
+                assert True
+                return
+            assert False
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            assert False
+
+    @pytest.mark.skip
+    def test_signup_TC132(self, driver: WebDriver):
+        try:
+            login_page = LoginPage(driver)
+            login_page.open()
+            login_page.check_url("signin")
+            login_page.click_button("로그인")
+            login_page.check_url("login")
+
+            driver.find_element(By.XPATH, '//a[contains(text(),"비밀번호를 잊으셨나요?")]').click()
+            login_page.check_url("u/reset-password")
+
+            driver.find_element(By.NAME, "email").send_keys(LoginData["username"])
+            driver.find_element(By.XPATH, '//button[contains(text(),"계속")]').click()
+
+            driver.find_element(By.XPATH, '//button[contains(text(),"Resend email")]').click()
+            class_value="비밀번호를 잊어버리셨나요?"
+
+            get_text = driver.find_element(By.XPATH, f"//h1[contains(text(),'{class_value}')]").text
+
+            if class_value in get_text :
+                print(get_text)
+                assert True
+                return
+            assert False
+
         except Exception as e:
             print(f"오류 발생: {e}")
             assert False
